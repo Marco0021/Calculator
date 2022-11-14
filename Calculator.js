61eddbca612846cffdb2b5783ac4fcb372b8678d
Marco <Marco@example.com>
2022-11-14 16:00:01 +0100

    aggiunti tasti ed effetto hover

---
+++ Calculator.js
@@ -0,0 +1,27 @@
+function add(num, num2) {
+  let result = num + num2;
+  return result;
+}
+console.log(add(1, 5));
+
+function subtract(num, num2) {
+  let result = num - num2;
+  return result;
+}
+console.log(subtract(5, 1));
+
+function multiply(num, num2) {
+  let result = num * num2;
+  return result;
+}
+console.log(multiply(2, 5));
+
+function divide(num, num2) {
+  let result = num / num2;
+  return result;
+}
+console.log(divide(8, 4));
+
+function operate() {
+  add(4, 10);
+}
