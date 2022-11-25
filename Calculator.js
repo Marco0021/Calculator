a73cca634a49add57700c2111b881537a1920d74
Marco <Marco@example.com>
2022-11-25 15:55:07 +0100

    visualizzazione numeri display

--- Calculator.js
+++ Calculator.js
@@ -1,12 +1,5 @@
-// let button = document.querySelector(".button");
-// button.addEventListener("click", example);
-
-// function example() {
-//   document.getElementById("#space").innerHTML += "example</br>";
-// }
-
-// function add(numValue, numSecondValue) {
-//   let result = numValue + numSecondValue;
+// function add(operand1, operand2) {
+//   let result = operand1 + operand2;
 //   return result;
 // }
 
@@ -25,100 +18,51 @@
 //   return result;
 // }
 
-// function operate() {
-//   add(numberValue, numberSecondValue);
-//   subtract(numberValue, numberSecondValue);
-//   multiply(numberValue, numberSecondValue);
-//   divide(numberValue, numberSecondValue);
+function operate() {
+  // add(operand1, operand2);
+  // subtract(numberValue, numberSecondValue);
+  // multiply(numberValue, numberSecondValue);
+  // divide(numberValue, numberSecondValue);
+}
+
+let displayValue = 0;
+let operand1;
+let operand2;
+let operateMath;
+
+let display = document.querySelector("#display");
+
+function keyAll() {
+  let keys = Object.values(document.getElementsByClassName("numKey"));
+  keys.forEach((key) => {
+    key.addEventListener("click", (e) => choiceNum(e.target));
+  });
+}
+
+keyAll();
+
+function choiceNum(firstNum, secondNum) {
+  document.querySelector("#display").textContent += firstNum.textContent;
+  currentValue = firstNum.textContent;
+  nextValue = secondNum;
+  // operand1;
+  console.log(secondNum);
+  console.log(firstNum.textContent);
+}
+// let result = currentValue.concat(nextValue);
+// nextValue = element.textContent;
+// operand1 = currentValue + nextValue;
+// if (operand1 > currentValue) {
+//   console.log(operand1);
+//   console.log(element.textContent);
+//   console.log(nextValue);
+// console.log(currentValue);
+//   return operand1;
 // }
 
-// let display = (document.querySelector(".display").innerHTML = "0");
-// let button = document.getElementsBy(".numCalc");
-// button.addEventListener("click", createNumber);
-
-// function createNumber() {
-//   let number7 = (document.querySelector(".display").innerHTML = "ciao");
-displayValue = 0;
-let display = (document.querySelector(
-  "#display"
-).innerHTML = `${displayValue}`);
-
-// function numbers() {
-//   let key7 = document.querySelector(".key7").addEventListener("click", () => {
-//     displayValue = 7;
-//     let display = (document.querySelector(
-//       ".display"
-//     ).innerHTML = `${displayValue}`);
-//   });
-
-let key8 = document.querySelector(".key8").addEventListener("click", () => {
-  activeNum("7");
-});
-
-function activeNum(choiceNum) {
-  numValue = 8;
-  console.log(choiceNum);
-  if (choiceNum === 8) {
-    display.innerHTML = `${numValue}`;
-    console.log(choiceNum);
-  }
-}
-
-function clearcontent(elementID) {
-  document.getElementById(elementID).innerHTML = "";
-}
-
-// let key9 = document.querySelector(".key9").addEventListener("click", () => {
-//   let display = (document.querySelector(".display").innerHTML = "9");
-// });
-// let keyPlus = document
-//   .querySelector(".keyplus")
-//   .addEventListener("click", () => {
-//     let display = (document.querySelector(".display").innerHTML = "+");
-//   });
-// let key4 = document.querySelector(".key4").addEventListener("click", () => {
-//   let display = (document.querySelector(".display").innerHTML = "4");
-// });
-// let key5 = document.querySelector(".key5").addEventListener("click", () => {
-//   let display = (document.querySelector(".display").innerHTML = "5");
-// });
-// let key6 = document.querySelector(".key6").addEventListener("click", () => {
-//   let display = (document.querySelector(".display").innerHTML = "6");
-// });
-// let keyLess = document
-//   .querySelector(".keyless")
-//   .addEventListener("click", () => {
-//     let display = (document.querySelector(".display").innerHTML = "-");
-//   });
-// let key1 = document.querySelector(".key1").addEventListener("click", () => {
-//   let display = (document.querySelector(".display").innerHTML = "1");
-// });
-// let key2 = document.querySelector(".key2").addEventListener("click", () => {
-//   let display = (document.querySelector(".display").innerHTML = "2");
-// });
-// let key3 = document.querySelector(".key3").addEventListener("click", () => {
-//   let display = (document.querySelector(".display").innerHTML = "3");
-// });
-// let keyMultiplier = document
-//   .querySelector(".keymultiplier")
-//   .addEventListener("click", () => {
-//     let display = (document.querySelector(".display").innerHTML = "*");
-//   });
-// let keyDelete = document
-//   .querySelector(".keydelete")
-//   .addEventListener("click", () => {
-//     let display = (document.querySelector(".display").innerHTML = "0");
-//   });
-// let key0 = document.querySelector(".key0").addEventListener("click", () => {
-//   let display = (document.querySelector(".display").innerHTML = "0");
-// });
-// let keySame = document
-//   .querySelector(".keysame")
-//   .addEventListener("click", () => {
-//     let display = (document.querySelector(".display").innerHTML = "=");
-//   });
-// let keyDivider = document
-//   .querySelector(".keydivider")
-//   .addEventListener("click", () => {
-//     let display = (document.querySelector(".display").innerHTML = "/");
-//   });
+// function operateAll() {
+//   let keyplus = document
+//     .querySelector("#keyplus")
+//     .addEventListener("click", add());
+//   // console.log(keyplus);
+// }
