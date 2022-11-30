ae1dd71f0ada5cf7957b41da25ef1fe847584a13
Marco <Marco@example.com>
2022-11-30 16:57:45 +0100

    visibili operandi sul display

--- Calculator.js
+++ Calculator.js
@@ -1,68 +1,38 @@
-// function add(operand1, operand2) {
-//   let result = operand1 + operand2;
-//   return result;
-// }
-
-// function subtract(numValue, numSecondValue) {
-//   let result = numValue - numSecondValue;
-//   return result;
-// }
-
-// function multiply(numValue, numSecondValue) {
-//   let result = numValue * numSecondValue;
-//   return result;
-// }
-
-// function divide(numValue, numSecondValue) {
-//   let result = numValue / numSecondValue;
-//   return result;
-// }
-
-function operate() {
-  // add(operand1, operand2);
-  // subtract(numberValue, numberSecondValue);
-  // multiply(numberValue, numberSecondValue);
-  // divide(numberValue, numberSecondValue);
-}
+let display = document.querySelector("#display");
+let numbers = document.querySelectorAll(".btn");
+let operators = document.querySelectorAll(".operator");
+let clear = document.querySelector("#keydelete");
+let equals = document.querySelector("#equals");
 
 let displayValue = 0;
-let operand1;
-let operand2;
-let operateMath;
+let operand1 = "";
+let operand2 = "";
+let operator = "";
 
-let display = document.querySelector("#display");
-
-function keyAll() {
-  let keys = Object.values(document.getElementsByClassName("numKey"));
-  keys.forEach((key) => {
-    key.addEventListener("click", (e) => choiceNum(e.target));
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    if (operator == "") {
+      operand1 += e.target.innerText;
+      display.textContent = operand1;
+      displayValue = display.textContent;
+      console.log(`op1: ${operand1}`);
+    } else {
+      operand2 += e.target.innerText;
+      display.textContent = operand1 + operator + operand2;
+      displayValue = operand1 + operator + operand2;
+      console.log(`op2: ${operand2}`);
+    }
+    console.log(`dv 2:${displayValue}`);
   });
-}
+});
 
-keyAll();
-
-function choiceNum(firstNum, secondNum) {
-  document.querySelector("#display").textContent += firstNum.textContent;
-  currentValue = firstNum.textContent;
-  nextValue = secondNum;
-  // operand1;
-  console.log(secondNum);
-  console.log(firstNum.textContent);
-}
-// let result = currentValue.concat(nextValue);
-// nextValue = element.textContent;
-// operand1 = currentValue + nextValue;
-// if (operand1 > currentValue) {
-//   console.log(operand1);
-//   console.log(element.textContent);
-//   console.log(nextValue);
-// console.log(currentValue);
-//   return operand1;
-// }
-
-// function operateAll() {
-//   let keyplus = document
-//     .querySelector("#keyplus")
-//     .addEventListener("click", add());
-//   // console.log(keyplus);
-// }
+operators.forEach((operatorBtn) => {
+  operatorBtn.addEventListener("click", (e) => {
+    if (e.target.innerText !== "=") {
+      operator = e.target.innerText;
+      displayValue = operand1 + operator;
+      display.textContent = displayValue;
+      console.log(`dv :${displayValue}`);
+    }
+  });
+});
