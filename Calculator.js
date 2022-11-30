f070f56cc0de15b4a6b4cf284dd28365f2af2000
Marco <Marco@example.com>
2022-11-30 18:50:52 +0100

    aggiunti calcoli delle operazioni finali

--- Calculator.js
+++ Calculator.js
@@ -9,30 +9,77 @@ let operand1 = "";
 let operand2 = "";
 let operator = "";
 
-numbers.forEach((number) => {
-  number.addEventListener("click", (e) => {
-    if (operator == "") {
-      operand1 += e.target.innerText;
-      display.textContent = operand1;
-      displayValue = display.textContent;
-      console.log(`op1: ${operand1}`);
-    } else {
-      operand2 += e.target.innerText;
-      display.textContent = operand1 + operator + operand2;
-      displayValue = operand1 + operator + operand2;
-      console.log(`op2: ${operand2}`);
-    }
-    console.log(`dv 2:${displayValue}`);
+function calculator() {
+  numbers.forEach((number) => {
+    number.addEventListener("click", (e) => {
+      if (operator == "") {
+        operand1 += e.target.innerText;
+        display.textContent = operand1;
+        displayValue = display.textContent;
+        console.log(`op1: ${operand1}`);
+      } else {
+        operand2 += e.target.innerText;
+        display.textContent = operand1 + operator + operand2;
+        displayValue = operand1 + operator + operand2;
+        console.log(`op2: ${operand2}`);
+      }
+      console.log(`dv 2:${displayValue}`);
+    });
   });
-});
 
-operators.forEach((operatorBtn) => {
-  operatorBtn.addEventListener("click", (e) => {
-    if (e.target.innerText !== "=") {
-      operator = e.target.innerText;
-      displayValue = operand1 + operator;
-      display.textContent = displayValue;
-      console.log(`dv :${displayValue}`);
-    }
+  operators.forEach((operatorBtn) => {
+    operatorBtn.addEventListener("click", (e) => {
+      if (e.target.innerText !== "=") {
+        operator = e.target.innerText;
+        displayValue = operand1 + operator;
+        display.textContent = displayValue;
+        console.log(`dv :${displayValue}`);
+      }
+    });
   });
-});
+
+  equals.addEventListener("click", calcResult);
+
+  function calcResult() {
+    switch (operator) {
+      case "+":
+        display.textContent = add(operand1, operand2);
+        displayValue = display.textContent;
+        console.log(displayValue);
+        break;
+      case "-":
+        display.textContent = subtract(operand1, operand2);
+        displayValue = display.textContent;
+        console.log(displayValue);
+        break;
+      case "*":
+        display.textContent = multiply(operand1, operand2);
+        displayValue = display.textContent;
+        console.log(displayValue);
+        break;
+      case "/":
+        display.textContent = divider(operand1, operand2);
+        displayValue = display.textContent;
+        console.log(displayValue);
+        break;
+    }
+  }
+
+  function add(operand1, operand2) {
+    result = +operand1 + +operand2;
+    return result;
+  }
+  function subtract(operand1, operand2) {
+    result = operand1 - operand2;
+    return result;
+  }
+  function divider(operand1, operand2) {
+    result = +operand1 / +operand2;
+    return result;
+  }
+  function multiply(operand1, operand2) {
+    result = +operand1 * +operand2;
+    return result;
+  }
+}
+calculator();
